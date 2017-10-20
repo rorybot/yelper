5c05cef79060d3ee447468bc91850d4511700294
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-20 15:37:13 +0100

    User email displayed as logged in

--- spec/features/homepage_feature_spec.rb
+++ spec/features/homepage_feature_spec.rb
@@ -2,6 +2,15 @@ describe 'site homepage' do
 
   feature 'visiting the homepage' do
 
+
+      include Devise::Test::IntegrationHelpers
+
+      before do
+        user = User.create email: 'tansaku@gmail.com', password: '12345678', password_confirmation: '12345678'
+        p user
+        sign_in(user)
+      end
+
     it 'has a link that takes you to the restaurant list' do
       visit '/'
       expect(page).to have_content('Crowd-sourced reviews about local businesses')
--- spec/features/restaurant_index_spec.rb
+++ spec/features/restaurant_index_spec.rb
@@ -1,6 +1,22 @@
 require "rails_helper"
 
 feature "restaurants" do
+
+  include Devise::Test::IntegrationHelpers
+
+  before do
+    user = User.create email: 'tansaku@gmail.com', password: '12345678', password_confirmation: '12345678'
+    p user
+    sign_in(user)
+  end
+
+  context 'when first visiting the page' do
+    scenario 'should see who is logged in' do
+      visit '/'
+      expect(page).to have_content('tansaku@gmail.com is logged in')
+    end
+  end
+
   context "restaurants are visible in the list of restaurants" do
     scenario "should display 2 restaurants after creating them" do
        Restaurant.create(name: "Pizza Express", rating: 4, address: "London", description: "Pizzas")
