cd22f3ffd419e2e6e81a398a9cb640d0a5c9a7b2
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-10-20 13:24:11 +0100

    Passed tests with users

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,9 +13,17 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment",__FILE__)
+require 'rspec/rails'
+require "capybara/rspec"
+include Capybara::DSL
+
 
 
 RSpec.configure do |config|
+
+
 
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -96,4 +104,5 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  
 end
